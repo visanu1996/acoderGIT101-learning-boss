33daaacb4d2e2d644f4e2287ce038cba53eba307
visanu1996 <workland1@live.com>
2026-01-21 20:54:19 +0700

    first commit from Git101 session

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # acoderGIT101-learning-boss
 This is sample repository for GIT101 learning
+
+### Edit conten ###
---
+++ src/test.ts
@@ -0,0 +1,8 @@
+let var_string = "Hello World! "
+let var_string2 = "Learning Git"
+
+function greetWorld(name:string){
+    console.log(name + var_string + var_string2)
+}
+
+greetWorld("Berk Rising ")
