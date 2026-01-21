1f15e727bc01c149bc72997567485a64558cd047
TheImages <nitisak.mooltreesri@gmail.com>
2026-01-21 21:42:47 +0700

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -2,7 +2,7 @@ let var_string = "Hello World! "
 let var_string2 = "Learning Git"
 
 function greetWorld(name:string){
-    console.log(name + var_string + var_string2)
+    console.log(name + var_string + var_string2+ "Mui Edited !")
 }
 
 greetWorld("Berk Rising ")
